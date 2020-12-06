3f633df5168dc787c11d17e821d5fedbd3a85a56
Mark Love <marklove93@gmail.com>
2020-12-06 17:09:47 +1100

    rework day 3

--- 3/day3.py
+++ 3/day3.py
@@ -1,32 +1,17 @@
-from functools import reduce
+from math import prod
 
 trees = list(map(lambda x: x.strip(), open('input.txt', 'r').readlines()))
 
 
-def slope(dx, dy):
-    hits = 0
-    x = 0
-    y = 0
+def path(d):
+    hits, x, y = 0, 0, 0
     while y < len(trees):
         line = trees[y]
-        y += dy
-        if line[x] == '#':
-            hits += 1
-        x += dx
-        if x >= len(line):
-            x -= len(line)
+        hits += 1 if line[x] == '#' else 0
+        x, y = x+d[0], y+d[1]
+        x -= len(line) if x >= len(line) else 0
     return hits
 
 
-collisions = [
-    slope(1, 1),
-    slope(3, 1),
-    slope(5, 1),
-    slope(7, 1),
-    slope(1, 2)
-]
-product = reduce(lambda i, j: i * j, collisions)
-print(
-    f'Part 1: {collisions[1]}\n'
-    f'Part 2: {product}'
-)
+print(f'Part 1: {path((3, 1))}')  # 299
+print(f'Part 2: {prod(map(path, [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]))}')  # 3621285278
